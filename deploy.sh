c27cac549faa8e8f1ddad5c198ba2fe61fc33144
Mads Andersen <bjerre+gitlab@gmail.com>
2019-12-28 04:07:46 +0100

    rename bucket to deploy

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+rm -rf public
+hugo
 gsutil defacl ch -u allUsers:READER gs://www.madsandersen.com
 gsutil web set -m index.html -e 404.html gs://www.madsandersen.com
 gsutil -m rsync -R public gs://www.madsandersen.com
